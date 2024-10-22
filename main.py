5c3782ece04745391d47e4fb4abd78463f5abc91
Lucas1362 <jamersontenorio88@gmail.com>
2024-10-22 20:19:00 -0300

    retirada da interface para recomeçar

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 import tkinter as tk
 
 from cleaner import DataCleanerApp  # Funcionalidades
-from interface import DataCleanerInterface  # Interface
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     
     cleaner_app = DataCleanerApp(root)  # Instancia o DataCleanerApp com funcionalidades
-    interface_app = DataCleanerInterface(root, cleaner_app)  # Passa o DataCleanerApp para a interface
+    # Passa o DataCleanerApp para a interface
     
     root.mainloop()
