762d3cf37caa0411f2a07a77e401f9ea5034c955
Lucas1362 <jamersontenorio88@gmail.com>
2025-01-03 20:10:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import tkinter as tk
-<<<<<<< HEAD
-from interface import DataCleanerApp
-from estilo import aplicar_estilo_botao, aplicar_estilo_entry
-
-if __name__ == "__main__":
-    root = tk.Tk()  # Cria a janela principal
-    app = DataCleanerApp(root)  # Inicializa a aplicação
-    root.mainloop()  # Inicia o loop da interface gráfica
-=======
 from cleaner import DataCleanerApp
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = DataCleanerApp(root)
     root.mainloop()
->>>>>>> parent of 4bf3b48 (criação de uma pasta apaneas para a interface com o intuito de diexar o codigo mais maleavel)
